daa3c3478ef880f932b2e8d39d8417100ef64011
Felipe <felipeishara@Hotmail.com>
2020-06-24 18:20:55 -0300

    handlebars-express instalados e rotas adm funfando

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const bodyParser = require('body-parser')
 //const mongoose = require('mongoose')
 const handlebars = require('express-handlebars')
 const app = express()
+const admin = require('./routes/admin')
 //config
+    //bodyparser
+    app.use(bodyParser.urlencoded({extended: true})) 
+    app.use(bodyParser.json())    
     //handlebars
     app.engine('hadlebars', handlebars({defaultLayout: 'main'})) 
     app.set('view engine', 'handlebars') 
-    //bodyparser
-    app.use(bodyParser.unlencoded({extended:false})) 
-    app.use(bodyParser.json())
+    
 //Rotas
-
+    app.use('/admin', admin)
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {console.log("Servidor rodando! ")})
